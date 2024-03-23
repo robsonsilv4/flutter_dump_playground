a31b31965c7b709ff5f388e89fd75e11a97b0446
robsonsilv4 <robsonsilv410@gmail.com>
2024-03-23 12:03:55 -0300

    chore: add initial flutter project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flutter Dump Playground
+
+Experiment, Learn, and Explore! A diverse collection of samples and projects to boost your Flutter skills.
---
+++ android/app/src/main/kotlin/dev/robsonsilva/flutter_dump_playground/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.robsonsilva.flutter_dump_playground
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
