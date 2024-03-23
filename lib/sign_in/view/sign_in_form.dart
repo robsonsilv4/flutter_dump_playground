02b9baa82480f4c621af04730f0fe64e2e7ca6d8
robsonsilv4 <robsonsilv410@gmail.com>
2024-03-23 14:41:33 -0300

    feat(sign_in): extract widgets and add email validation

--- lib/sign_in/view/sign_in_form.dart
+++ lib/sign_in/view/sign_in_form.dart
@@ -9,26 +9,67 @@ class SignInForm extends StatelessWidget {
   Widget build(BuildContext context) {
     const sizes = MoonSizes.sizes;
     return Form(
+      autovalidateMode: AutovalidateMode.onUserInteraction,
       child: Column(
         mainAxisSize: MainAxisSize.min,
         children: [
-          MoonFormTextInput(
-            textInputSize: MoonTextInputSize.lg,
-            hintText: 'Email',
-          ),
+          const _EmailInput(),
           Gap(sizes.x2s),
           MoonFormTextInput(
             textInputSize: MoonTextInputSize.lg,
             hintText: 'Password',
           ),
           Gap(sizes.xs),
-          MoonFilledButton(
-            width: double.maxFinite,
-            label: const Text('Sign In'),
-            onTap: () {},
-          ),
+          const _SignInButton(),
         ],
       ),
+    );
+  }
+}
+
+class _EmailInput extends StatelessWidget {
+  const _EmailInput();
+
+  static final _emailRegExp = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+
+  @override
+  Widget build(BuildContext context) {
+    return MoonFormTextInput(
+      hintText: 'Email',
+      textInputSize: MoonTextInputSize.lg,
+      keyboardType: TextInputType.emailAddress,
+      textInputAction: TextInputAction.next,
+      validator: _validateEmail,
+    );
+  }
+
+  String? _validateEmail(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Email is required';
+    }
+    if (!_emailRegExp.hasMatch(value)) {
+      return 'Invalid email';
+    }
+    return null;
+  }
+}
+
+class _SignInButton extends StatelessWidget {
+  const _SignInButton();
+
+  @override
+  Widget build(BuildContext context) {
+    return MoonFilledButton(
+      width: double.maxFinite,
+      label: const Text('Sign In'),
+      onTap: () {
+        if (Form.of(context).validate()) {
+          MoonToast.show(
+            context,
+            label: const Text('Home is not implemented yet!'),
+          );
+        }
+      },
     );
   }
 }
