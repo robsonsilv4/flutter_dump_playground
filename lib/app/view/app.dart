80a74052786ce00e53077bc216ff4129df565344
robsonsilv4 <robsonsilv410@gmail.com>
2024-03-23 12:54:35 -0300

    style: add moon design

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:moon_design/moon_design.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Dump Playground',
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
+      theme: ThemeData.light().copyWith(
+        extensions: <ThemeExtension<dynamic>>[
+          MoonTheme(tokens: MoonTokens.light),
+        ],
+      ),
+      darkTheme: ThemeData.dark().copyWith(
+        extensions: <ThemeExtension<dynamic>>[
+          MoonTheme(tokens: MoonTokens.dark),
+        ],
+      ),
+      home: const Scaffold(
         body: Center(
           child: Text('Welcome to Dump Playground!'),
         ),
